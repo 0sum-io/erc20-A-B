1ab90cccddc8455c1f10c22fe550e14c758e53db
leon-do <leondo123@gmail.com>
2024-04-29 22:34:57 -0400

    deploy erc20s

---
+++ src/TokenB.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20{
+    constructor() ERC20("TokenB", "TKNB") {
+        _mint(msg.sender, 999 * 10 ** 18);
+    }
+
+    function mint() public {
+        _mint(msg.sender, 10 ** 18);
+    }
+}
