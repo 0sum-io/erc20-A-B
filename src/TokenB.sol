637f8d499782a7613b547f9da43c667d1e9376dc
leon-do <leondo123@gmail.com>
2025-02-06 13:46:26 -0500

    mint more

--- src/TokenB.sol
+++ src/TokenB.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20{
     constructor() ERC20("TokenB", "TKNB") {
-        _mint(msg.sender, 999 * 10 ** 18);
+        _mint(msg.sender, 9999 * 10 ** 18);
     }
 
     function mint() public {
